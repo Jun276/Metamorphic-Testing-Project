c14a496c006fdc46f3ee6c14038da6e41298ec4f
Jun276 <wnsgur3592@gmail.com>
2026-06-02 14:28:36 +0900

    회전 각도 변경

--- metamorphic_testing/lib/metamorphic_relation.py
+++ metamorphic_testing/lib/metamorphic_relation.py
@@ -6,20 +6,20 @@ from scipy import ndimage
 # 반시계 방향 회전(준)
 # def T(dset):
 #    new_dset = []
-#    rotate = 5
+#    rotate = 9
 #    for d in dset:
 #        new_d = ndimage.rotate(d, rotate, reshape=False)
 #        new_dset.append(new_d)
 #    return np.array(new_dset)
 
 # 시계 방향 회전(봉)
-# def T(dset):
-#     new_dset = []
-#     rotate = -5
-#     for d in dset:
-#         new_d = ndimage.rotate(d, rotate, reshape=False)
-#         new_dset.append(new_d)
-#     return np.array(new_dset)
+def T(dset):
+    new_dset = []
+    rotate = -9
+    for d in dset:
+        new_d = ndimage.rotate(d, rotate, reshape=False)
+        new_dset.append(new_d)
+    return np.array(new_dset)
 
 # 흐림처리(준)
 # def T(dset):
