894d9a62af158812ecccc934473f92637563e7bb
Jun276 <wnsgur3592@gmail.com>
2026-06-08 12:32:16 +0900

    쓰레기 이미지 삭제, 회전+밝게 t함수 및 점+ 흐림 T함수 추가

--- metamorphic_testing/lib/metamorphic_relation.py
+++ metamorphic_testing/lib/metamorphic_relation.py
@@ -88,6 +88,53 @@ from scipy import ndimage
 
 #     return np.array(new_dset)
 
+# 0. 회전, 밝게
+# def T(dset):
+#     new_dset = []
+#     rotate = 9
+#     brightness = -0.05
+
+#     for d in dset:
+#         new_d = ndimage.rotate(d, rotate, reshape=False)
+#         new_d = new_d + brightness
+#         new_d = np.clip(new_d, 0.0, 1.0)
+#         new_dset.append(new_d)
+
+#     return np.array(new_dset)
+
+# 0. 점, 흐림
+# import random
+
+# def add_points(img):
+
+#     new_img = img.copy()
+
+#     y = random.randint(0, 27)
+#     x = random.randint(0, 27)
+
+#     for dy in [-1, 0, 1]:
+#         for dx in [-1, 0, 1]:
+
+#             ny = y + dy
+#             nx = x + dx
+
+#             if 0 <= ny < 28 and 0 <= nx < 28:
+#                 new_img[ny, nx, 0] = 1.0
+
+#     return new_img
+
+# def T(dset):
+#     new_dset = []
+#     sigma = 0.5
+
+#     for d in dset:
+#         new_d = add_points(d)
+#         new_d = ndimage.gaussian_filter(new_d, sigma=sigma)
+#         new_dset.append(new_d)
+
+#     return np.array(new_dset)
+
+
 
 def E(source_y, follow_y):
     result = []
